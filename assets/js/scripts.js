4387dfa57df23334f4eb064460cec95d2185974a
iqlipsechar@gmail.com <iqlipsechar@gmail.com>
2021-01-06 21:30:43 +0530

    Added client side form valiadtions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,4 +11,25 @@ $("input,select").focusout(function(e){
     $(`label[for="${id}"]`).parent().css({backgroundColor:"white",transition:'all 0.5s'})
     $(`#${id}`).css({backgroundColor:"white",transition:'all 0.5s'})
 })
-//
+//Taking input from form , making todos and adding validation
+$('#addTask').click(function(e){
+    e.preventDefault();
+    var description=$("#description").val();
+    if(description.trim()==null || description.trim()==""){
+            alert("Add a description")
+            return;
+    }
+    var category=$("#category").val();
+    console.log(category)
+    if(category==null || category==""){
+            alert("Choose a category")
+            return ;
+    }
+    var date=$("#date").val();
+    if(date==null || date==""){
+            alert("Choose a date")
+            return;
+    }
+    
+
+})
